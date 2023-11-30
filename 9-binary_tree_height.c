d98308c46b0be162066ee299a63163a8fe6c4881
Sarahkiss215 <skisiangani5@gmail.com>
2023-11-30 04:08:04 -0800

    Change the variable i and j to pointer left and right respectively

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,12 +12,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t i = 0;
 	size_t j = 0;
 
-	if (tree == NULL || (tree->i == NULL && tree->j == NULL))
+	if (tree == NULL || (tree->left == NULL && tree->rigth == NULL))
 	{
 		return (0);
 	}
-	i = binary_tree_height(tree->i);
-	j = binary_tree_height(tree->j);
+	i = binary_tree_height(tree->left);
+	j = binary_tree_height(tree->right);
 
 	return ((i > j ? i : j) + 1);
 }
